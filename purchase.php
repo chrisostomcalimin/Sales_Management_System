53f3e5742bafd6f92192bffeb607f92692925018
Mr.Calmin <chrisostom.calimin23@mustudent.ac.tz>
2026-06-10 02:41:28 -0700

    feat: Add purchase_date input to purchase form processor

--- purchase.php
+++ purchase.php
@@ -10,11 +10,18 @@ $quantity = $_POST['quantity'];
 
 $cost = $_POST['cost'];
 
+$purchase_date = $_POST['purchase_date'];
+
 $total = $quantity * $cost;
 
 
-$sql = "INSERT INTO purchases(item_name, quantity, cost, total)
-VALUES('$item', '$quantity', '$cost', '$total')";
+// Validate date format
+if (!strtotime($purchase_date)) {
+    die("Invalid date format. Please use YYYY-MM-DD format.");
+}
+
+$sql = "INSERT INTO purchases(item_name, quantity, cost, total, purchase_date)
+VALUES('$item', '$quantity', '$cost', '$total', '$purchase_date')";
 
 
 mysqli_query($conn, $sql);
