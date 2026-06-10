e6d8f1f323a8930e10068d4c9b6dda505f0dc999
Mr.Calmin <chrisostom.calimin23@mustudent.ac.tz>
2026-06-10 02:43:12 -0700

    feat: Add expense_date input to expense form processor

--- expense.php
+++ expense.php
@@ -1,3 +1,4 @@
+
 ```php
 
 <?php
@@ -8,9 +9,16 @@ $description = $_POST['description'];
 
 $amount = $_POST['amount'];
 
+$expense_date = $_POST['expense_date'];
 
-$sql = "INSERT INTO expenses(description, amount)
-VALUES('$description', '$amount')";
+
+// Validate date format
+if (!strtotime($expense_date)) {
+    die("Invalid date format. Please use YYYY-MM-DD format.");
+}
+
+$sql = "INSERT INTO expenses(description, amount, expense_date)
+VALUES('$description', '$amount', '$expense_date')";
 
 
 mysqli_query($conn, $sql);
